124285e95a3f6e5fbb20697a2390ea246e7bd951
davidqdo <davidqdo@csu.fullerton.edu>
2022-08-04 18:02:16 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@ const skillsContent = document.getElementsByClassName('skills__content'),
 function toggleSkills(){
     let itemClass = this.parentNode.className
 
-    for(i = 0; i > skillsContent.length; i++){
+    for(i = 0; i < skillsContent.length; i++){
         skillsContent[i].className = 'skills__content skills__close'
     }
     if(itemClass === 'skills__content skills__close'){
